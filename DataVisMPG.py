785c97956b032667b915e9e55e8329c24928033d
Carlos <cjuarez84@student.ccc.edu>
2026-05-03 18:09:45 -0500

    for data vis project

---
+++ DataVisMPG.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import the file such that we can pull from it
+file_path = 'auto-mpg.data'
+#time to format
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight','Acceleration', 'Model Year', 'Origin']
+#data frame
+df = pd.read_csv(file_path, names = column_names, na_values='?', comment='\t', sep=" ", skipinitialspace=True)
+
+#Visualization 1 Scatter plot (Weight vs MPG)
+plt.figure(figsize=(8,5))
+sns.scatterplot(x='Weight', y='MPG', data = df, color='blue', alpha=0.6)
+plt.title('Impact of Vehicle Weight on Fuel Efficiency')
+plt.xlabel('Weight (lbs)')
+plt.ylabel('Miles per Gallon (MPG)')
+plt.grid(True, linestyle='--', alhpa=0.7)
+#plt.savefig('weight_vs_mpg.png')
+plt.show()
+
+#Visualization 2 box plot cyls vs mpg
+plt.figure(figsize=(8,5))
+sns.boxplot(x='Cylinders', y='MPG', data = df, paletter='Set2')
+plt.title('MPG Distribution by Number of Cylinders')
+plt.xlabel('Number of Cylinders')
+plt.ylabel('Miles per Gallon (MPG)')
+#plt.savefig('cuylinders_vs_mpg.png')
+plt.show()
+
+print(df.head())
